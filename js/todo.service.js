a6fcb24ec0ce2de03210dfbe1ba8ee85aff7c0e0
Jared Bilton <cantops@jareds-mbp.attlocal.net>
2016-08-07 17:31:11 -0400

    modified service

---
+++ js/todo.service.js
@@ -0,0 +1,43 @@
+(function() {
+  'use strict';
+
+
+function TodoService ($http) {
+  var API = 'http://tiny-tiny.herokuapp.com/collections/myTodo';
+
+  function create(todo){
+    return $http.post(API, todo).then(function (response) {
+      return response.data;
+    });
+  }
+  function retrieve(){
+    return $http.get(API).then(function (response) {
+      return response.data.splice(0, 10);
+    });
+  }
+
+
+  function update(todo){
+    return $http.put(API + '/' + todo._id, todo).then(function (response) {
+      return response.data;
+    });
+  }
+  function remove(todo){
+      return $http.delete(API + '/' + todo._id).then(function (response) {
+        return response.data;
+      });
+  }
+
+  return {
+    create: create,
+    retrieve: retrieve,
+    update: update,
+    remove: remove
+};
+}
+
+angular
+  .module ('app')
+  .factory ('TodoService', TodoService);
+
+}());
